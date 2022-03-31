d4fbc77bf0f88487fb955a9b117e14c3cfdab869
14920251 <85061916+14920251@users.noreply.github.com>
2022-03-31 23:16:31 +0530

    iot_mvvm22

--- app/src/main/java/com/example/iot_api/util/constants.kt
+++ app/src/main/java/com/example/iot_api/util/constants.kt
@@ -1,7 +1,9 @@
 package com.example.iot_api.util
 
+import com.example.iot_api.BuildConfig
+
 class constants {
     companion object{
-        const val BASE_URL = "https://makeup-api.herokuapp.com/api/v1/"
+        const val BASE_URL = BuildConfig.KEY
     }
 }
