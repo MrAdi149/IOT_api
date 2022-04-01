44b80ae9b8027d7b8a40b02f5b53b2cd3a1224d1
14920251 <85061916+14920251@users.noreply.github.com>
2022-04-01 18:18:00 +0530

    iot_mvvm23

--- app/src/main/java/com/example/iot_api/util/constants.kt
+++ app/src/main/java/com/example/iot_api/util/constants.kt
@@ -1,9 +1,10 @@
 package com.example.iot_api.util
 
+import android.os.Build.VERSION_CODES.M
 import com.example.iot_api.BuildConfig
 
 class constants {
     companion object{
-        const val BASE_URL = BuildConfig.KEY
+        const val BASE_URL = BuildConfig.ApiKey
     }
 }
